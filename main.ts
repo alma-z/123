7592ef1ab697d57d41f499716353b7686b4c0373
alma-z <zenoalma14@gmail.com>
2025-11-25 15:00:04 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showIcon(IconNames.Heart)
+music.play(music.stringPlayable("C5 B A C5 B A C5 A ", 120), music.PlaybackMode.UntilDone)
+basic.showString("good morning")
+basic.forever(function () {
+	
+})
